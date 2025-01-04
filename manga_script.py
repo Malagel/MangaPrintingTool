cc7a5c578104fcb7ad4654d6f7e87c5f4fc62fbf
Nicolás Mariángel <cubemasterr@gmail.com>
2025-01-04 20:15:44 -0300

    added validations

--- manga_script.py
+++ manga_script.py
@@ -59,7 +59,6 @@ def scan_and_sort_images(input_folder, target_width_cm, cover):
     manga_width = get_average_page_width(input_folder)
 
     image_paths = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith(('.jpg', '.png'))]
-    print(f"extracting images from {cbz_files}")
     print(f"Found {len(image_paths)} images in {cbz_files}")
 
     # Organize the paths
@@ -74,16 +73,17 @@ def scan_and_sort_images(input_folder, target_width_cm, cover):
             image_name = os.path.basename(image)  
             if not page_regex.search(image_name):
                 print(f"Error: couldn't find page number in {image_name}")
-                print("Please rename the files to include the page number (e.g. p1.jpg, p2.jpg or 1.jpg, 2.jpg, etc.).")
+                print("Please rename the files to include the page number (e.g. p1, p2 or 1, 2 ,3, etc). All pages must be numbered the same way.") 
                 raise ValueError(f"Invalid file: {image}")
         
         image_paths.sort(key=lambda x: int(page_regex.search(os.path.basename(x)).group(1)))
 
-    print("cutting and resizing images...")
+    print("Cutting and resizing images...")
 
     # Sort the images, save the cut and uncut pages in numbered order and delete the original images while also resizing them
 
     counter = 0
+    double_page_paths = []
     for image_path in image_paths:
 
         print(f"Processing image number {counter}...", end="\r")
@@ -103,6 +103,7 @@ def scan_and_sort_images(input_folder, target_width_cm, cover):
                     dpi=(300, 300),
                 )
 
+                double_page_paths.append(os.path.join(input_folder, f"{str(counter).zfill(3)}.png"))
                 counter += 2
             else:
                 with Image.open(image_path) as img:
@@ -127,15 +128,13 @@ def scan_and_sort_images(input_folder, target_width_cm, cover):
     if cover:
         image_paths.remove(image_paths[0])
 
-    return image_paths
+    return image_paths, double_page_paths
 
 def extract_file(file, output_folder):
     with zipfile.ZipFile(file, 'r') as zip_ref:
         zip_ref.extractall(output_folder)
 
 def get_paper_dimensions(paper_size):
-
-
     paper_dimensions = {
         "A4": (210, 297),
         "Letter": (216, 279),
@@ -145,15 +144,12 @@ def get_paper_dimensions(paper_size):
     return paper_dimensions[paper_size]	
 
 def add_blank_page(image_paths, input_folder):
-    last_page_num = 0
-    for path in image_paths:
-        filename = os.path.basename(path)
-        if filename.endswith(".png"):
-            page_num = int(filename.split('.')[0]) 
-            last_page_num = max(last_page_num, page_num)
-
-    new_page_num = last_page_num + 1
-    blank_page_path = os.path.join(input_folder, f"{str(new_page_num).zfill(3)}.png")
+    counter = 0
+    while True:
+        blank_page_path = os.path.join(input_folder, f"blank_page_{counter}.png")
+        if not os.path.exists(blank_page_path):
+            break
+        counter += 1
 
     with Image.open(image_paths[1]) as img:
         img_width, img_height = img.size
@@ -167,61 +163,119 @@ def add_blank_page(image_paths, input_folder):
         blank_page.save(blank_page_path)
         
     return blank_page_path
+
+def validate_printing_order(image_paths, double_page_paths, pages_order):
+    print(double_page_paths)
+
+    if pages_order == "left" and double_page_paths:
+        total_pages = len(image_paths)
+
+        for path in double_page_paths:
+            index = image_paths.index(path)
+
+            if index == 0 or index == total_pages - 1:
+                raise ValueError("The first or last page can't be a double page. Please change the order of the pages.")
+
+            if index % 2 == 0:
+                blank_page_path = add_blank_page(image_paths, input_folder='input')
+                image_paths.insert(0, blank_page_path)
+                break
     
-def create_pdf(image_paths, output_folder, paper_size, manga_size):
+    print("order of double pages validated")
+    return image_paths
+
+def validate_divisibility_by_4(image_paths):
     pages_total = len(image_paths)
 
     if pages_total < 4:
         raise ValueError("Not enough pages to create a PDF. Please provide at least 4 pages.")
     
-    if pages_total % 4 != 0:
-        print("adding page...")
+    if pages_total % 4 == 0:
+        return image_paths
+    
+    # First attempt: Add 2 blank pages
+
+    blank_pages_added = 0
+    while len(image_paths) % 4 != 0 and blank_pages_added < 2:
         blank_page_path = add_blank_page(image_paths, input_folder='input')
         image_paths.append(blank_page_path)
-        pages_total += 1
+        blank_pages_added += 1
+        print("page added")
+
+    if len(image_paths) % 4 == 0:
+        return image_paths
+    
+    # Second attempt: Remove up to 4 pages
 
     pages_deleted = 0
-    while pages_total % 4 != 0 and pages_deleted <= 3:
-        image_paths.pop()  
-        print("deleting page")
-        pages_total -= 1
+    while len(image_paths) % 4 != 0 and pages_deleted < 4:
+        image_paths.pop()
         pages_deleted += 1
+        print("page deleted")
 
-    if pages_total % 4 != 0:
+    if len(image_paths) % 4 != 0:
         raise ValueError("""
-                        The program couldn't create a PDF, because the number of the pages must be divisible by 4. 
-                        A blank page was added, then two pages were deleted, but it wasn't enough. 
-                        Please delete or add pages manually.
-                         """)
+            The program couldn't create a PDF because the number of pages must be divisible by 4. 
+            Two blank pages were added, and up to four pages were removed, but it wasn't enough. 
+            Please delete or add pages manually until the total is divisible by 4.
+        """)
+
+    print("divisibility by 4 validated")
+    return image_paths
     
+def create_pdf(image_paths, output_folder, paper_size, manga_size, pages_order, double_page_paths):
+
+    print("validating printing order...")
+    image_paths = validate_printing_order(image_paths, double_page_paths, pages_order)
+    
+    print("validating divisibility by 4...")
+    image_paths = validate_divisibility_by_4(image_paths)
+
     print(image_paths)
+
     pdf = FPDF(unit="cm", format=paper_size)
 
     print("Creating PDF...")
+
+def welcome_message():
+    print("--------------------------------------------------------------------------------------------------------")
+    print("                                Welcome to the Manga/Book/Comic Printing Tool")
+    print("--------------------------------------------------------------------------------------------------------")
+    print("               This tool will help you print manga or books in the correct order and size.")
+    print("          Make sure to place your pages in the 'input' folder, and just one manga/book per use.")
+    print("                             They can be .zip, .cbz, .jpg, or .png files.")
+    print("--------------------------------------------------------------------------------------------------------")
+    print("         IMPORTANT: the files in the 'input' folder WILL be modified, so make sure to have a backup.")
+    print("--------------------------------------------------------------------------------------------------------")
 
 def main():
     input_folder = "input"
     output_folder = "output"
 
-    print("--------------------------------------------------------------------------------------------------------")
-    print("                                  Welcome to the Manga Printing Tool")
-    print("--------------------------------------------------------------------------------------------------------")
-    print("                  This tool will help you print manga in the correct order and size.")
-    print("           Make sure to place your pages in the 'input' folder, and just one manga per use.")
-    print("                             They can be .zip, .cbz, .jpg, or .png files.")
-    print("--------------------------------------------------------------------------------------------------------")
-    print("         IMPORTANT: the files in the 'input' folder WILL be modified, so make sure to have a backup.")
-    print("--------------------------------------------------------------------------------------------------------")
+    welcome_message()
     
+    while True:
+        pages_order = input("Choose the order of the pages that you will be reading in (left, or right): ").strip().lower()
+        if pages_order in ["left", "right"]:
+            break
+
     while True:
         paper_size = input("Please choose paper size to print (A4, Letter, or A5): ").strip()
         if paper_size in ["A4", "Letter", "A5"]:
             break    
 
     while True:
-        manga_size = input("Please choose the manga width in centimeters (usually it's 12cm): ").strip()
+        manga_size = input("Please choose the width of the manga/book in centimeters (usually it's 12cm) or type 'full': ").strip()
         if manga_size.isnumeric() and int(manga_size) > 0 and int(manga_size) < 30: 
             manga_size = int(manga_size)
+            break
+        elif manga_size == "full":
+            if paper_size == "A4":
+                manga_size = 14
+            elif paper_size == "Letter":
+                manga_size = 13
+            elif paper_size == "A5":
+                manga_size = 10
             break
     
     while True:
@@ -232,13 +286,12 @@ def main():
         elif cover == "n":
             cover = False
             break
-
     print("--------------------------------------------------------------------------------------------------------")
 
     try:
-        images_paths = scan_and_sort_images(input_folder, manga_size, cover)    
+        images_paths, double_page_paths = scan_and_sort_images(input_folder, manga_size, cover)    
 
-        create_pdf(images_paths, output_folder, paper_size, manga_size)
+        create_pdf(images_paths, output_folder, paper_size, manga_size, pages_order, double_page_paths)
         
         print()
         print(f"PDF saved in: {output_folder}")
